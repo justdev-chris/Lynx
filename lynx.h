6ba38d878cb83a4dc0b1be78ba2f4136f4ec5360
Chris <morenochristopher580@gmail.com>
2026-02-08 14:36:18 -0600

    Update lynx.h

--- lynx.h
+++ lynx.h
@@ -3,7 +3,7 @@
 
 typedef enum {
     TOKEN_ROAR, TOKEN_STALK, TOKEN_SET, TOKEN_POUNCE, TOKEN_CHASE, 
-    TOKEN_STALK_PACK, TOKEN_ID, TOKEN_NUMBER, TOKEN_STRING,
+    TOKEN_STALK_PACK, TOKEN_HUNT, TOKEN_HELP, TOKEN_ID, TOKEN_NUMBER, TOKEN_STRING,
     TOKEN_EQUALS, TOKEN_PLUS, TOKEN_MINUS, TOKEN_STAR, TOKEN_SLASH,
     TOKEN_GREATER, TOKEN_LESS, TOKEN_EOF, TOKEN_ERROR
 } TokenType;
@@ -14,15 +14,14 @@ typedef struct {
     int length;
 } Token;
 
-// Core Engine Functions
 void initScanner(const char* source);
 Token scanToken();
 Token peekToken();
 void parse_statement();
 void runFile(const char* path);
 
-// Memory System
 void pounce_store(const char* name, double value);
 double pounce_get(const char* name);
+void pounce_list(); // For the Hunt command
 
 #endif
