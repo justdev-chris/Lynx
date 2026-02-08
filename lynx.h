19724b59ace80fe8de0a72493da08c9cee9ab6f7
Chris <morenochristopher580@gmail.com>
2026-02-08 14:45:12 -0600

    Update lynx.h

--- lynx.h
+++ lynx.h
@@ -3,7 +3,8 @@
 
 typedef enum {
     TOKEN_ROAR, TOKEN_STALK, TOKEN_SET, TOKEN_POUNCE, TOKEN_CHASE, 
-    TOKEN_STALK_PACK, TOKEN_HUNT, TOKEN_HELP, TOKEN_ID, TOKEN_NUMBER, TOKEN_STRING,
+    TOKEN_STALK_PACK, TOKEN_HUNT, TOKEN_HELP, TOKEN_STASH,
+    TOKEN_ID, TOKEN_NUMBER, TOKEN_STRING,
     TOKEN_EQUALS, TOKEN_PLUS, TOKEN_MINUS, TOKEN_STAR, TOKEN_SLASH,
     TOKEN_GREATER, TOKEN_LESS, TOKEN_EOF, TOKEN_ERROR
 } TokenType;
@@ -22,6 +23,7 @@ void runFile(const char* path);
 
 void pounce_store(const char* name, double value);
 double pounce_get(const char* name);
-void pounce_list(); // For the Hunt command
+void pounce_list();
+void pounce_stash(const char* path);
 
 #endif
