a6a5950b5659bfb06c92500e55be48b0404342cd
Chris <morenochristopher580@gmail.com>
2026-02-08 14:03:58 -0600

    Update lynx.h

--- lynx.h
+++ lynx.h
@@ -2,9 +2,8 @@
 #define lynx_h
 
 typedef enum {
-    TOKEN_ROAR, TOKEN_STALK, TOKEN_SET, TOKEN_POUNCE,
-    TOKEN_ID, TOKEN_NUMBER, TOKEN_EQUALS, TOKEN_PLUS, 
-    TOKEN_MINUS, TOKEN_EOF, TOKEN_ERROR
+    TOKEN_ROAR, TOKEN_SET, TOKEN_ID, TOKEN_NUMBER, 
+    TOKEN_EQUALS, TOKEN_PLUS, TOKEN_MINUS, TOKEN_EOF, TOKEN_ERROR
 } TokenType;
 
 typedef struct {
@@ -13,9 +12,9 @@ typedef struct {
     int length;
 } Token;
 
-// Function Prototypes
 void initScanner(const char* source);
 Token scanToken();
+Token peekToken();
 void pounce_store(const char* name, double value);
 double pounce_get(const char* name);
 void parse_statement();
