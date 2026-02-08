31d95b40d74dc6dea1de28cd3d7baaa5c111e6ae
Chris <morenochristopher580@gmail.com>
2026-02-08 13:57:56 -0600

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+#include "lynx.h"
 
+int main() {
+    char line[1024];
+    printf("Lynx Engine v1.0 - Ready to Hunt\n");
+    while (1) {
+        printf("lynx > ");
+        if (!fgets(line, sizeof(line), stdin)) break;
+        initScanner(line);
+        parse_statement();
+    }
+    return 0;
+}
