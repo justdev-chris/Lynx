8f4bb11dbaab87c785f89d2aa0da7eca397a6dc9
Chris <morenochristopher580@gmail.com>
2026-02-08 14:06:46 -0600

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
+#ifdef _WIN32
+#include <io.h>
+#define isatty _isatty
+#else
+#include <unistd.h>
+#endif
 #include "lynx.h"
 
 int main() {
     char line[1024];
     while (1) {
-        if (isatty(0)) printf("lynx > "); // Only print prompt if interactive
+        // Now works on Windows and Linux!
+        if (isatty(0)) printf("lynx > "); 
+        
         if (!fgets(line, sizeof(line), stdin)) break;
         initScanner(line);
         parse_statement();
